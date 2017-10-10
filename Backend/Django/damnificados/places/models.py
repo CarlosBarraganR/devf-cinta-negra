fdc8d686cbc15ead9a85c7ba9ada60920ea61af6
Carlos Barragan <starttheshooting@hotmail.com>
2017-10-09 22:05:16 -0500

    created personasHasPlaces model

--- Backend/Django/damnificados/places/models.py
+++ Backend/Django/damnificados/places/models.py
@@ -3,10 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from personas.models import Personas
+
 # Create your models here.
+
+STATUS = (("1","Actual"),("0","Ya no"))
 
 # Model Places
 class Places(models.Model):
     place_name = models.CharField(max_length=45)
     street_name = models.CharField(max_length=45)
     city =  models.CharField(max_length=45)
+
+# Model PersonasHasPlaces 
+class PersonasHasPlaces(models.Model):
+    date = models.DateField()
+    status = models.CharField(choices=STATUS, max_length=20)
+    places_id = models.ForeignKey(Places)
+    personas_id = models.ForeignKey(Personas)
